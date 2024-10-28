fd8ead74e6e3e7344df2029d0128779c5c3357e1
Juan <38849891+xoltia@users.noreply.github.com>
2024-10-28 16:48:34 -0500

    Add faster youtube info option

--- src/yt-dlp.ts
+++ src/yt-dlp.ts
@@ -1,3 +1,5 @@
+import ytdl from "@distube/ytdl-core";
+
 export type VideoInfo = {
     id: string;
     title: string;
@@ -8,9 +10,52 @@ export type VideoInfo = {
 
 export type YtDlpOptions = {
     ytDlpPath: string;
+    disableYtdlCore?: boolean;
 };
 
+function pickBestThumbnail(thumbnails: ytdl.thumbnail[]): ytdl.thumbnail | undefined {
+    if (thumbnails.length === 0)
+        return undefined;
+
+    let bestRes = 0;
+    let bestThumbnail = thumbnails[0];
+
+    for (const thumbnail of thumbnails) {
+        const res = thumbnail.width * thumbnail.height;
+        if (res > bestRes) {
+            bestRes = res;
+            bestThumbnail = thumbnail;
+        }
+    }
+
+    return bestThumbnail;
+}
+
+function checkForYouTubeId(url: string): string | null {
+    const ytVideoLinkRegex = /(?:youtube\.com\/watch\?v=|youtube\.com\/live\/|youtu\.be\/)([a-zA-Z0-9_-]+)/;
+    const match = url.match(ytVideoLinkRegex);
+    return match ? match[1] : null;
+}
+
 export async function getVideoInfo(url: string, options: YtDlpOptions): Promise<VideoInfo> {
+    const ytId = checkForYouTubeId(url);
+    if (!options.disableYtdlCore) {
+        try {
+            const info = await ytdl.getBasicInfo(url);
+            if (info.videoDetails.thumbnails.length === 0)
+                throw new Error('No thumbnails found');
+            return {
+                id: info.videoDetails.videoId,
+                title: info.videoDetails.title,
+                url: info.videoDetails.video_url,
+                duration: parseInt(info.videoDetails.lengthSeconds),
+                thumbnail: pickBestThumbnail(info.videoDetails.thumbnails)!.url,
+            };
+        } catch (e) {
+            console.error('Failed to get video info with ytdl-core:', e);
+        }
+    }
+
     const proc = Bun.spawn([
         options.ytDlpPath,
         '--print-json',
